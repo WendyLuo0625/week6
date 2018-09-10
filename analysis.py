0d15004825dbc64944217c194931bee9c7ca4ed5
WendyLuo0625 <wendyluo1990@163.com>
2018-09-10 22:12:05 +0800

    task18

---
+++ analysis.py
@@ -0,0 +1,18 @@
+import json
+import pandas as pd
+import os
+
+def analysis(file, user_id):
+    times = 0 
+    minutes = 0
+    if not os.path.exists(file):
+        return 0
+    else:
+        df = pd.read_json(file)
+        times = df[df['user_id'] == user_id].count()
+        minutes = df[df['user_id'] == user_id]['minutes'].sum()
+        return times, minutes
+
+if __name__ == '__main__':
+    a = analysis("user_study.json", 223684)
+    print(a)
